acfd780bff8cbd58cd0961d1c74a6ab3215f12f2
Richard Green <richardjgreen@users.noreply.github.com>
2017-03-13 23:24:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -18,19 +18,46 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Test Azure Traffic Manager Template</h1>
+  
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Test Azure Traffic Manager Template</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Test Azure Traffic Manager Template</h1>
+        <p class="lead">This sample site demonstrates how to verify that Azure Traffic Manager is working with multiple Web App endpoints as expected.</p>
+		
+		<p>With this site deployed, use an online tool such as Pingdom Free Page Test (<a href="https://tools.pingdom.com/">https://tools.pingdom.com/</a>) and test the URL of this Azure Traffic Manager instance.</p>
 	
-	<p>This sample site demonstrates how to verify that Azure Traffic Manager is working with multiple Web App endpoints as expected.</p>
-	<p>With this site deployed, use an online tool such as Pingdom Free Page Test (<a href="https://tools.pingdom.com/">https://tools.pingdom.com/</a>) and test the URL of this Azure Traffic Manager instance.</p>
-	
-	<h2>How to Perform the Test</h2>
-	<p>Visit the web testing site and enter the URL of this Azure Traffic Manager instance. Set the test from location to a location close to one of your endpoints and repeat the test a second time using a location close to your other endpoint.</p>
-	<p>In the Pingdom FPT results, you will see a preview of the site that was loaded in each location.</p>
-	
-	<h1>This page was loaded from the <?php echo getenv("WEBAPP_LOCATION") ?> instance of the Web App.</h1>
-	
-	<!-- We can also print the instance name to a HTML comment so that it's hidden in the browser Development Tools for debugging when we need it -->
-	<!-- This page was loaded from the <?php echo getenv("WEBAPP_LOCATION") ?> instance of the Web App. -->
+		<h2>How to Perform the Test</h2>
+		<p>Visit the web testing site and enter the URL of this Azure Traffic Manager instance. Set the test from location to a location close to one of your endpoints and repeat the test a second time using a location close to your other endpoint.</p>
+		<p>In the Pingdom FPT results, you will see a preview of the site that was loaded in each location.</p>
+		
+		<h1>This page was loaded from the <?php echo getenv("WEBAPP_LOCATION") ?> instance of the Web App.</h1>
+		
+		<!-- We can also print the instance name to a HTML comment so that it's hidden in the browser Development Tools for debugging when we need it -->
+		<!-- This page was loaded from the <?php echo getenv("WEBAPP_LOCATION") ?> instance of the Web App. -->
+			
+      </div>
+
+    </div><!-- /.container -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
