4ee70031fd0a0514450b1959027d316c14ffbe19
Richard Green <richardjgreen@users.noreply.github.com>
2017-03-13 23:19:09 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Test Azure Traffic Manager Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Test Azure Traffic Manager Template</h1>
+	
+	<p>This sample site demonstrates how to verify that Azure Traffic Manager is working with multiple Web App endpoints as expected.</p>
+	<p>With this site deployed, use an online tool such as Pingdom Free Page Test (<a href="https://tools.pingdom.com/">https://tools.pingdom.com/</a>) and test the URL of this Azure Traffic Manager instance.</p>
+	
+	<h2>How to Perform the Test</h2>
+	<p>Visit the web testing site and enter the URL of this Azure Traffic Manager instance. Set the test from location to a location close to one of your endpoints and repeat the test a second time using a location close to your other endpoint.</p>
+	<p>In the Pingdom FPT results, you will see a preview of the site that was loaded in each location.</p>
+	
+	<h1>This page was loaded from the <?php echo getenv("WEBAPP_LOCATION") ?> instance of the Web App.</h1>
+	
+	<!-- We can also print the instance name to a HTML comment so that it's hidden in the browser Development Tools for debugging when we need it -->
+	<!-- This page was loaded from the <?php echo getenv("WEBAPP_LOCATION") ?> instance of the Web App. -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
